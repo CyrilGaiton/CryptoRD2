532ff3cedb4bfb9c1403760a95346c6baab1f716
Cyril <cyril.gaiton@etud.univ-tln.fr>
2018-11-17 10:35:40 +0100

    Initial commit, minimal interface, no exceptions

---
+++ app/src/main/java/fr/masterdapm/cgaiton611/tpcrypto/MainActivity.java
@@ -0,0 +1,145 @@
+package fr.masterdapm.cgaiton611.tpcrypto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText msg;
+    EditText mdp;
+    TextView cle;
+    TextView res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        msg = findViewById(R.id.msg);
+        mdp = findViewById(R.id.mdp);
+        cle = findViewById(R.id.cle);
+        res = findViewById(R.id.res);
+    }
+
+
+    public void chiffrer(View v) {
+        byte[] bytes = Chiffre(mdp.getText().toString().toCharArray(), msg.getText().toString().getBytes());
+        String cryptoB64 = Base64.encodeToString(bytes, 0, bytes.length, Base64.NO_PADDING | Base64.NO_WRAP);
+        res.setText(cryptoB64);
+    }
+
+    public void dechiffrer(View v){
+        byte[] cryptogram;
+        cryptogram = Base64.decode(msg.getText().toString(), Base64.NO_PADDING | Base64.NO_WRAP);
+        byte[] dechiffre = Dechiffre(mdp.getText().toString().toCharArray(), cryptogram);
+        res.setText(new String(dechiffre, StandardCharsets.UTF_8));
+    }
+
+
+    public SecretKey GenAESKeyFromPass(char[] password, byte[] sel, int iter, int taille) {
+        SecretKey secretKeySpec = null;
+        try {
+
+            KeySpec keySpec = new PBEKeySpec(password, sel, iter, taille);
+            SecretKeyFactory secretKeyFactory = null;
+            secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            SecretKey secretKey = secretKeyFactory.generateSecret(keySpec);
+            secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return secretKeySpec;
+    }
+
+
+    public byte[] Chiffre(char[] password, byte[] plaintext) {
+        ByteBuffer byteBuffer = null;
+        try {
+
+            SecureRandom secureRandom = new SecureRandom();
+            byte[] sel = new byte[128/8];
+            secureRandom.nextBytes(sel);
+
+            SecretKey secretKey = GenAESKeyFromPass(mdp.getText().toString().toCharArray(), sel, 10000, 128);
+            cle.setText(Base64.encodeToString(secretKey.getEncoded(), 0, 128/8, Base64.NO_PADDING | Base64.NO_WRAP));
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] chiffre = cipher.doFinal(msg.getText().toString().getBytes());
+
+            Cipher cipherIV = Cipher.getInstance("AES/ECB/NoPadding");
+            cipherIV.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] chiffreIV = cipherIV.doFinal(cipher.getIV());
+
+            byteBuffer = ByteBuffer.allocate(256/8 + chiffre.length);
+            byteBuffer.put(chiffreIV);
+            byteBuffer.put(sel);
+            byteBuffer.put(chiffre);
+
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return byteBuffer.array();
+    }
+
+    public byte[] Dechiffre(char[] password, byte[] cryptogram){
+        byte[] dechiffre = null;
+        try {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(cryptogram.length);
+            byteBuffer.put(cryptogram);
+            byteBuffer.rewind();
+
+            byte[] chiffreIV = new byte[128/8];
+            byte[] sel = new byte[128/8];
+            byte[] chiffre = new byte[cryptogram.length - 256/8];
+            byteBuffer.get(chiffreIV, 0, 128/8);
+            byteBuffer.get(sel, 0, 128/8);
+            byteBuffer.get(chiffre, 0, cryptogram.length - 256/8);
+
+            SecretKey secretKey = GenAESKeyFromPass(mdp.getText().toString().toCharArray(), sel, 10000, 128);
+            cle.setText(Base64.encodeToString(secretKey.getEncoded(), 0, 128/8, Base64.NO_PADDING | Base64.NO_WRAP));
+
+            Cipher cipherIV = Cipher.getInstance("AES/ECB/NoPadding");
+            cipherIV.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] dechiffreIV = cipherIV.doFinal(chiffreIV);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(dechiffreIV);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+            dechiffre = cipher.doFinal(chiffre);
+
+
+
+        } catch (IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+        return dechiffre;
+    }
+
+
+}
